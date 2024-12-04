6a0c8e46d581179989ca0c0b5d663d6dcdebbca8
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-12-04 12:18:00 +0200

    + base api class

---
+++ src/utils/base-api/BaseApi.ts
@@ -0,0 +1,50 @@
+import { HTTPMethod } from "../../enums/enums";
+import { ValueOf } from "../../types/types";
+
+type queryParamsType = Record<string, string>
+
+
+class BaseApi {
+    protected baseUrl: string;
+
+    public constructor(baseUrl: string){
+        this.baseUrl = baseUrl
+    }
+
+    public async load(
+        path: string,
+        method: ValueOf<typeof HTTPMethod>,
+        payload: BodyInit | null,
+        headers: Headers,
+        queryParams: queryParamsType
+    ){
+        const fullUrl = this.getFullEndpoint(path, queryParams)
+
+        const response = await fetch(fullUrl, {
+            method,
+            body: payload,
+            headers: headers
+        })
+        return response
+    }
+
+    protected getFullEndpoint(
+        path: string, 
+        queryParams: queryParamsType
+    ): string{
+        let fullUrl = `${this.baseUrl}${path}`
+        if(Object.keys(queryParams).length !== 0){
+            fullUrl += '?'
+        }
+        Object.keys(queryParams).forEach((param, index) => {
+            if(index !== 0){
+                fullUrl += '&'
+            }
+            fullUrl += queryParams[param]
+        })
+        return fullUrl
+    }
+
+}
+
+export { BaseApi };
