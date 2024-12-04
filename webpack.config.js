3923d78503b9f06b8527026037c0e88c24bf9538
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-12-04 10:36:18 +0200

    + configured using css modules in project

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: './src/index.tsx',
@@ -8,7 +9,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: ['.tsx', '.ts', '.js', '.css']
   },
   module: {
     rules: [
@@ -16,12 +17,32 @@ module.exports = {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         use: 'babel-loader'
-      }
+      },
+      {
+        test: /\.module\.css$/, 
+        use: [
+          'style-loader', 
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true, 
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/, 
+        exclude: /\.module\.css$/, 
+        use: ['style-loader', 'css-loader'],
+      },
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
-    })
+    }),
+    new MiniCssExtractPlugin({
+        filename: "[name].[contenthash].css",
+      }),
   ]
 };
