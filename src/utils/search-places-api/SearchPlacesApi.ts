70aa45b6728f2010cc20fa3efbf092079b691373
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-12-04 17:06:07 +0200

    + handling response error

--- src/utils/search-places-api/SearchPlacesApi.ts
+++ src/utils/search-places-api/SearchPlacesApi.ts
@@ -2,6 +2,7 @@ import { HTTPMethod } from "../../enums/enums"
 import { PlacesResponse, type Coordinates } from "../../types/types"
 import { BaseApi } from "../base-api/BaseApi"
 import { SearchPlacesApiPath } from "./enums/search-places-api-path.enum"
+import { API_KEY } from "./constants/constants"
 
 class SearchPlacesApi extends BaseApi{
 
@@ -10,17 +11,23 @@ class SearchPlacesApi extends BaseApi{
     }
 
     async getPlacesNearby(coordinates: Coordinates){
+        const headers = new Headers()
+        headers.append('Authorization', API_KEY)
 
         const response = await this.load({
             path:  SearchPlacesApiPath.PLACES_NEARBY,
             method: HTTPMethod.GET,
             payload: null,
-            headers: new Headers(),
+            headers,
             queryParams: {
                 latitude: `${coordinates.latitude}`,
                 longitude: `${coordinates.longitude}`
             }
         })
+        if(!response.ok){
+            const errorData = await response.json()
+            throw new Error(errorData.message || 'Something went wrong')
+        }
         
         const places = (await response.json()) as PlacesResponse
         return places
