81b7777e1441e5c3c2e87a295735999829b2d7d2
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-12-04 17:28:12 +0200

    + processing response

--- src/services/places-nearby/PlacesNearby.ts
+++ src/services/places-nearby/PlacesNearby.ts
@@ -7,18 +7,33 @@ type RequestError = {
 
 class PlacesNearby {
 
-    async getPlacesNearby(coordinates: Coordinates): Promise<PlacesResponse | RequestError>{
+    async getPlacesNearby(coordinates: Coordinates): Promise<Place[] | RequestError>{
         // TODO coordinates validation
         try{
             const response: PlacesResponse = await searchPlacesApi.getPlacesNearby(coordinates);
-            return response
+            const processedPlaces = this.processPlacesNearby(response)
+            return processedPlaces
         }catch(e){
             return {
                 message: (e as Error).message
             }
         }
     }
-   
+
+    async processPlacesNearby(places: PlacesResponse): Promise<Place[]>{
+        const processedPlaces = places.results.map(place=>{
+            return {
+                name: place.name,
+                distance: place.distance,
+                coordinates: {
+                    latitude: place.geocodes.main.latitude,
+                    longitude: place.geocodes.main.longitude
+                },
+                category: place.categories && place.categories[0].name
+            }
+        })
+        return processedPlaces;
+    }
 
     // TODO method for processing raw data
 }
