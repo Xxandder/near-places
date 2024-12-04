dc7f46c438dad9ded4189ec6cfc33fd5316e07b0
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-12-04 20:38:33 +0200

    + refetching places with bigger radius if number of places is low

--- src/utils/search-places-api/SearchPlacesApi.ts
+++ src/utils/search-places-api/SearchPlacesApi.ts
@@ -3,7 +3,14 @@ import { type RawPlace, type Coordinates} from "../../types/types"
 import { type PlacesResponse } from "./types/types"
 import { BaseApi } from "../base-api/BaseApi"
 import { SearchPlacesApiPath } from "./enums/search-places-api-path.enum"
-import { API_KEY, MAX_AMOUNT_OF_PLACES, RADIUS } from "./constants/constants"
+import { 
+    API_KEY, 
+    MAX_AMOUNT_OF_PLACES, 
+    MIN_AMOUNT_OF_PLACES, 
+    MIN_RADIUS, 
+    MAX_RADIUS,
+    RADIUS_MULTIPLIER
+ } from "./constants/constants"
 import { placesApiMapper } from "./mappers/places-api-response.mapper"
 
 class SearchPlacesApi extends BaseApi{
@@ -15,27 +22,41 @@ class SearchPlacesApi extends BaseApi{
     async getPlacesNearby(
         coordinates: Coordinates, 
         limit=MAX_AMOUNT_OF_PLACES, 
-        radius=RADIUS
+        minRadius=MIN_RADIUS
     ): Promise<RawPlace[]>{
         const headers = new Headers()
         headers.append('Authorization', API_KEY)
 
-        const response = await this.load({
-            path:  SearchPlacesApiPath.PLACES_NEARBY,
-            method: HTTPMethod.GET,
-            payload: null,
-            headers,
-            queryParams: {
-                ll: `${coordinates.latitude}%2C${coordinates.longitude}`,
-                limit: `${limit}`,
-                radius: `${radius}`
+        let currentRadius = minRadius;
+        let responseJson;
+
+        do{
+            const response = await this.load({
+                path:  SearchPlacesApiPath.PLACES_NEARBY,
+                method: HTTPMethod.GET,
+                payload: null,
+                headers,
+                queryParams: {
+                    ll: `${coordinates.latitude}%2C${coordinates.longitude}`,
+                    limit: `${limit}`,
+                    radius: `${currentRadius}`
+                }
+            })
+            if(!response.ok){
+                const errorData = await response.json()
+                throw new Error(errorData.message || 'Something went wrong')
             }
-        })
-        if(!response.ok){
-            const errorData = await response.json()
-            throw new Error(errorData.message || 'Something went wrong')
+            responseJson = await response.json();
+
+            if(currentRadius > MAX_RADIUS){
+                break;
+            }
+            currentRadius = currentRadius * RADIUS_MULTIPLIER > MAX_RADIUS ? 
+                MAX_RADIUS : Math.round(currentRadius * RADIUS_MULTIPLIER)
         }
-        const places = placesApiMapper((await response.json()) as PlacesResponse)
+        while(responseJson.results.length < MIN_AMOUNT_OF_PLACES)
+        
+        const places = placesApiMapper(responseJson as PlacesResponse)
         return places
     }
 
--- src/utils/search-places-api/constants/constants.ts
+++ src/utils/search-places-api/constants/constants.ts
@@ -1,6 +1,17 @@
 const BASE_URL = 'https://api.foursquare.com/v3'
 const API_KEY = 'fsq3I/p8cgp1N0AavNbztaa62uI8Zi4mhiRJGEOZw4Uud5I='
 const MAX_AMOUNT_OF_PLACES = 15
-const RADIUS = 5000
+const MIN_AMOUNT_OF_PLACES = 3
+const MAX_RADIUS = 5000
+const MIN_RADIUS = 200
+const RADIUS_MULTIPLIER = 2
 
-export { BASE_URL, API_KEY, MAX_AMOUNT_OF_PLACES, RADIUS }
+export { 
+    BASE_URL, 
+    API_KEY, 
+    MAX_AMOUNT_OF_PLACES, 
+    MIN_AMOUNT_OF_PLACES,
+    MIN_RADIUS, 
+    MAX_RADIUS,
+    RADIUS_MULTIPLIER
+}
