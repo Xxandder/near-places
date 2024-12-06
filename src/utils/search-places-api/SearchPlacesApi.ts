50194596bfecc8625740a8b44a8b4548073bf57f
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-12-06 15:25:22 +0200

    + binary search algorithm for nearby places

--- src/utils/search-places-api/SearchPlacesApi.ts
+++ src/utils/search-places-api/SearchPlacesApi.ts
@@ -7,9 +7,9 @@ import {
     API_KEY, 
     MAX_AMOUNT_OF_PLACES, 
     MIN_AMOUNT_OF_PLACES, 
-    MIN_RADIUS, 
     MAX_RADIUS,
-    RADIUS_MULTIPLIER
+    INITIAL_RADIUS,
+    MAX_BATCH_SIZE
  } from "./constants/constants"
 import { placesApiMapper } from "./mappers/places-api-response.mapper"
 
@@ -19,44 +19,50 @@ class SearchPlacesApi extends BaseApi{
         super(baseUrl)
     }
 
+    
+
     async getPlacesNearby(
         coordinates: Coordinates, 
         limit=MAX_AMOUNT_OF_PLACES, 
-        minRadius=MIN_RADIUS
     ): Promise<RawPlace[]>{
         const headers = new Headers()
         headers.append('Authorization', API_KEY)
 
-        let currentRadius = minRadius;
+        let currentRadius = INITIAL_RADIUS;
+        let radius_low_boundary = 0;
+        let radius_upper_boundary = MAX_RADIUS;
         let responseJson;
 
-        do{
+        while(true){
             const response = await this.load({
-                path:  SearchPlacesApiPath.PLACES_NEARBY,
-                method: HTTPMethod.GET,
-                payload: null,
-                headers,
-                queryParams: {
-                    ll: `${coordinates.latitude}%2C${coordinates.longitude}`,
-                    limit: `${limit}`,
-                    radius: `${currentRadius}`
-                }
-            })
-            if(!response.ok){
-                console.log('here30')
-                const errorData = await response.json()
-                console.log('here34')
-                throw new Error(errorData.message || 'Something went wrong')
-            }
+                        path:  SearchPlacesApiPath.PLACES_NEARBY,
+                        method: HTTPMethod.GET,
+                        payload: null,
+                        headers,
+                        queryParams: {
+                            ll: `${coordinates.latitude}%2C${coordinates.longitude}`,
+                            limit: `30`,
+                            radius: `${currentRadius}`
+                        }
+                    })
             responseJson = await response.json();
 
-            if(currentRadius >= MAX_RADIUS){
+            if(responseJson.results.length < MIN_AMOUNT_OF_PLACES){
+                radius_low_boundary = currentRadius;
+                currentRadius += Math.round((radius_upper_boundary - currentRadius) / 2);
+               
+                continue;
+            }
+            else if(responseJson.results.length >= MAX_BATCH_SIZE){
+                radius_upper_boundary = currentRadius;
+                currentRadius -= Math.round((currentRadius - radius_low_boundary)  / 2);
+               
+                continue;
+            }
+            else{
                 break;
             }
-            currentRadius = currentRadius * RADIUS_MULTIPLIER > MAX_RADIUS ? 
-                MAX_RADIUS : Math.round(currentRadius * RADIUS_MULTIPLIER)
         }
-        while(responseJson.results.length < MIN_AMOUNT_OF_PLACES)
         const results: PlacesResponse['results'] = (responseJson as PlacesResponse).results
         
         const places = placesApiMapper(results.slice(0, MAX_AMOUNT_OF_PLACES))
