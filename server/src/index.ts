0d0dd636fd5aac666811020dafd7dad5228b8a51
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-12-07 07:32:48 +0200

    + cors for server app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,17 @@
-import express from 'express'
+import express, { Application } from 'express';
+import cors from 'cors';
 
-const app = express();
-const PORT = process.env.PORT || 5500;
+const app: Application = express();
 
+const PORT = process.env['PORT'] || 3000;
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello world' });
-});
-
-// Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
+  
+
+export default app;
