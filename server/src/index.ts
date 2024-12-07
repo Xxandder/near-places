92731e0acd392b7de2122148860677ea39c47568
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-12-07 08:10:36 +0200

    + nearby-places router

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 
+import { router as nearbyPlacesRouter } from './routes/nearby-places';
+
 const app: Application = express();
 
 const PORT = process.env['PORT'] || 3000;
@@ -8,6 +10,8 @@ const PORT = process.env['PORT'] || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(nearbyPlacesRouter)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
