79b8d035a2e9c129787c19ceda070805ba5e6bbb
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-12-08 13:17:51 +0200

    * fixex type conversion in schema variables

--- server/src/api/search-places-api/search-places-api.ts
+++ server/src/api/search-places-api/search-places-api.ts
@@ -2,13 +2,13 @@ import { BasePlacesApi } from "./base-search-places-api/BaseSearchPlacesApi";
 import { FoursquarePlacesSearchApi } from "./foursquare-search-places-api/FoursquarePlacesSearchApi";
 
 const currentApi = process.env['CURRENT_API']
-const maxAmountOfPlaces = process.env['MAX_AMOUNT_OF_PLACES']
-const minAmountOfPlaces = process.env['MIN_AMOUNT_OF_PLACES']
-const baseUrl = process.env['BASE_URL']
-const maxBatchSize = process.env['MAX_BATCH_SIZE']
-const maxRadius = process.env['MAX_RADIUS']
-const initialRadius = process.env['INITIAL_RADIUS']
-const apiKey = process.env['API_KEY']
+const maxAmountOfPlaces = Number(process.env['MAX_AMOUNT_OF_PLACES'])
+const minAmountOfPlaces = Number(process.env['MIN_AMOUNT_OF_PLACES'])
+const baseUrl = process.env['BASE_URL'] as string
+const maxBatchSize = Number(process.env['MAX_BATCH_SIZE'])
+const maxRadius = Number(process.env['MAX_RADIUS'])
+const initialRadius = Number(process.env['INITIAL_RADIUS'])
+const apiKey = process.env['API_KEY'] as string
 
 
 function createPlacesApi(): BasePlacesApi {
