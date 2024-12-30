90790dcc2af3d6a50dfb5017e0adedc5665c796c
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-12-30 12:59:43 +0200

    + common env config file for client folder

--- client/src/api/search-places-api/index.ts
+++ client/src/api/search-places-api/index.ts
@@ -1,10 +1,7 @@
-
 import { ApiPath } from '@/enums';
 import { SearchPlacesApi } from './SearchPlacesApi';
 
-const searchPlacesApiUrl = process.env['SEARCH_PLACES_API_URL']
-const searchPlacesApiPort = process.env['SEARCH_PLACES_API_PORT']
-
+import { searchPlacesApiPort, searchPlacesApiUrl } from '@/config';
 
 const searchPlacesApi = new SearchPlacesApi(`${searchPlacesApiUrl}:${searchPlacesApiPort}${ApiPath.NEARBY_PLACES}`);
 
---
+++ client/src/config/index.ts
@@ -0,0 +1,7 @@
+const searchPlacesApiUrl = process.env['SEARCH_PLACES_API_URL']
+const searchPlacesApiPort = process.env['SEARCH_PLACES_API_PORT']
+
+export { 
+    searchPlacesApiUrl,
+    searchPlacesApiPort
+}
