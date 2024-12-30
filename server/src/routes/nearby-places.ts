b709496ba17e7bd3be4cf64ba3efe888ff4aec20
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-12-30 14:31:16 +0200

    * using enum for validation source in route

--- server/src/routes/nearby-places.ts
+++ server/src/routes/nearby-places.ts
@@ -3,9 +3,10 @@ import { Router } from 'express'
 import { getNearbyPlaces } from '@/controllers/search-places';
 import { validationMiddleware, checkCache } from '@/middlewares';
 import { getNearbyPlacesSchema } from '@/validation-schema';
+import { ValidationSource } from '@/middlewares/enums';
 
 const router = Router()
 
-router.get('/', validationMiddleware(getNearbyPlacesSchema, 'query'), checkCache, getNearbyPlaces)
+router.get('/', validationMiddleware(getNearbyPlacesSchema, ValidationSource.QUERY), checkCache, getNearbyPlaces)
 
 export { router };
